298cd9640f356ea6f613b4fee716f5cfc1b5d397
Abdusattor <abdusattor1999@gmail.com>
2023-10-23 22:52:54 +0500

    Database postgres

--- config/settings.py
+++ config/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'rayhonto_db',
-    #     'USER': 'rayhonto_db_user',
-    #     'PASSWORD': 'j{WJ_^q$$oiO',
-    #     'HOST': 'localhost',
-    #     'PORT': 5432,
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rayhonto_db',
+        'USER': 'rayhonto_db_user',
+        'PASSWORD': 'j{WJ_^q$$oiO',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
 }
 
 JAZZMIN_SETTINGS = {
